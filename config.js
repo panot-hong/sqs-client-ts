8b165e751dbd094ee37ee159d0a636e5cf3df97b
panot-hong <mrpanot@gmail.com>
2019-08-27 12:57:10 +0700

    remove js file and correct queue name in create queue cmd

--- config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.QUEUE_NAME = `digested-content-queue`;
-exports.LOCAL = 1;
-exports.QUEUE_URL = 'http://localhost:4576';
-//# sourceMappingURL=config.js.map
